670ad23cbe30a9623ce598712bd35bd8ec6568d7
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-30 17:02:22 +1000

    Added grabber actor components to project

---
+++ Source/RelicRemovalist/Grabber.h
@@ -0,0 +1,25 @@
+// Copyright Jonathan Moallem 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class RELICREMOVALIST_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGrabber();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
